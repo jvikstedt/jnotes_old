1bc558c95a7fa1c2b082a694b05b135211b47055
Janne Vikstedt <janne.vikstedt@eficode.com>
2016-07-25 14:23:59 +0300

    UsersController and create action

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post 'user_token' => 'user_token#create'
       resources :notes
+      resources :users, only: [:create]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
